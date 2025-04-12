d3798fba80158ca97f1c6059343e6a38b4c86437
anupam <anupam.nitdgp14@gmail.com>
2025-04-12 14:15:47 +0530

    Initialized project.

---
+++ src/main/java/com/learner/transaction/SpringbootTransactionDemoProject6Application.java
@@ -0,0 +1,13 @@
+package com.learner.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTransactionDemoProject6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTransactionDemoProject6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/learner/transaction/SpringbootTransactionDemoProject6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learner.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTransactionDemoProject6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
